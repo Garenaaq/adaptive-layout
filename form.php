ba80574efd2002f6b0e0b9cea4a84d9ef727c64f
Garenaaq <GlebSpoke@gmail.com>
2024-09-05 18:16:29 +0300

    фиксы

--- form.php
+++ form.php
@@ -7,12 +7,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = trim(htmlspecialchars($name));
     $phone = trim(htmlspecialchars($phone));
 
+    $headers = "From: example@mail.ru\r\n" .
+        "Reply-To: example@mail.ru\r\n" .
+        "X-Mailer: PHP/" . phpversion();
 
-    if (mail("gleb.gavrilov.2014@mai.ru", "Заявка с сайта", "Номер телефона:" . $phone . ". Имя: " . $name)) {
+    // Устанавливаем заголовок с правильной кодировкой
+    header('Content-Type: text/html; charset=utf-8');
+
+    if (mail("rbru-metrika@yandex.ru", "Заявка с сайта", "Номер телефона:" . $phone . ". Имя: " . $name, $headers)) {
         echo "сообщение успешно отправлено";
     } else {
+        $errorMessage = error_get_last()['message'];
+        echo "Ошибка при отправке письма: " . $errorMessage;
         echo "при отправке сообщения возникли ошибки";
     }
 }
-
 ?>
